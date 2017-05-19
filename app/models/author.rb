035f6b8cb2255bc39e3071d753d6d240901627ec
Prasad Surase <prasadsurase@gmail.com>
2017-05-19 16:26:00 +0530

    Removed commented code

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,7 +4,6 @@ class Author < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 	has_many :commits, :dependent => :delete_all
 	has_many :repositories, :through => :AuthorRepositories
